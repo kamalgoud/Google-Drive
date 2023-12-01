00c61d55c0ea50854f00d4b018ece6d859121685
kamalgoud <kamalgoudkatta@gmail.com>
2023-12-01 14:47:20 +0530

    Git Repository Initialized

---
+++ src/test/java/com/mountblue/googledrive/GoogledriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountblue.googledrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogledriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
