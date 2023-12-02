81bc6ebc9688d71031bd1a60e8ef87614caf20fc
Gaurav Kumar <gaurav.kumar.26.1@mountblue.tech>
2023-12-02 11:33:32 +0530

    File uploading functionality done

--- src/main/java/com/mountblue/googledrive/service/FileService.java
+++ src/main/java/com/mountblue/googledrive/service/FileService.java
@@ -1,7 +1,43 @@
 package com.mountblue.googledrive.service;
 
+import com.mountblue.googledrive.entity.File;
+import com.mountblue.googledrive.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.attribute.FileAttribute;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class FileService {
+    private FileRepository fileRepository;
+
+    @Autowired
+    public FileService(FileRepository fileRepository){
+        this.fileRepository=fileRepository;
+    }
+
+    public void uploadFile(MultipartFile file) throws IOException {
+        File savefile = new File();
+
+        if (!file.isEmpty()) {
+            savefile.setFileName(file.getOriginalFilename());
+            savefile.setFileType(file.getContentType());
+            savefile.setContent(file.getBytes());
+            savefile.setSize(file.getSize());
+            savefile.setUploadDate(new Date());
+        } else {
+            throw new IllegalArgumentException("Uploaded file is empty");
+        }
+        fileRepository.save(savefile);
+    }
+
+    public List<File> allFiles(){
+        return fileRepository.findAll();
+    }
+
 }
