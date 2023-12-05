dfd04248a9e2b83566dc6e41f320b7282d71d1f6
kamalgoud <kamalgoudkatta@gmail.com>
2023-12-05 16:20:08 +0530

    Search and filter updated for different users

--- src/main/java/com/mountblue/googledrive/controller/FilterController.java
+++ src/main/java/com/mountblue/googledrive/controller/FilterController.java
@@ -2,13 +2,17 @@ package com.mountblue.googledrive.controller;
 
 
 import com.mountblue.googledrive.entity.File;
+import com.mountblue.googledrive.entity.Users;
 import com.mountblue.googledrive.service.FileService;
+import com.mountblue.googledrive.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -16,9 +20,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 @Controller
 public class FilterController {
@@ -26,16 +28,33 @@ public class FilterController {
     @Autowired
     private FileService fileService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/filter")
     public String filter( @RequestParam(name = "minSize", required = false) Long minSize,
                           @RequestParam(name = "maxSize", required = false) Long maxSize,
                           @RequestParam(name = "fileName", required = false) String fileName,
                           @RequestParam(name = "fileType", required = false) String fileType,
+                         Principal principal,
                          Model model){
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) principal;
+        Map<String, Object> userAttributes = oauthToken.getPrincipal().getAttributes();
+        String userEmail = (String) userAttributes.get("email");
+        Users user = userService.getUserByEmail(userEmail);
+
         if(fileType!=null && fileType.trim().equals("")){
             fileType = null;
         }
         List<File> filteredFiles = fileService.filterFiles(minSize, maxSize, fileName, fileType);
+
+        Iterator<File> iterator = filteredFiles.iterator();
+        while (iterator.hasNext()) {
+            File file = iterator.next();
+            if (file.getUser() != user) {
+                iterator.remove();  // Safe removal using Iterator
+            }
+        }
         model.addAttribute("files",filteredFiles);
         System.out.println(filteredFiles);
         return "filtered-files";
